8cbe23b445f753064c276263a836f63117c9d67d
Zahir Saiyed <zahir19saiyed@gmail.com>
2024-09-21 13:06:58 -0400

    Updating callback URL

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: 'https://network-akfc.onrender.com/auth/google/callback',
 }, (accessToken, refreshToken, profile, done) => {
   // Store the access token in the user object
   profile.accessToken = accessToken;
